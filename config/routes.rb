fe5db57a82fdab101554f06686ba48ab99b86b55
Bohdan Malets <bohdan.malets@gmail.com>
2016-02-29 15:46:26 +0200

    added default blocks view mode

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   
   resources :media
-  resources :synonyms
-  resources :sources
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root to: 'media#index'
 
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
-  root to: 'media#index'
 end
