b2ac3bf7a7122696fa8e16a16614a188754187e6
bohdankutkovy <kutkovybohdan@gmail.com>
2016-03-01 13:16:29 +0200

    adding notification method

--- app/models/media.rb
+++ app/models/media.rb
@@ -5,8 +5,9 @@ class Media
   field :type, type: String
   field :name, type: String
 
-  has_many :sources
-  has_many :synonyms
+  has_many   :sources
+  has_many   :synonyms
+  belongs_to :subscribe
 
   scope :movies,    -> { where(type: 'movie')  }
   scope :tv_series, -> { where(type: 'series') }
