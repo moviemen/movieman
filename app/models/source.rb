4b926289fe46be8d9f5ff8c108ccaf2adbadb87a
bohdankutkovy <kutkovybohdan@gmail.com>
2016-03-02 15:19:58 +0200

    notify logic for new releases

--- app/models/source.rb
+++ app/models/source.rb
@@ -4,23 +4,42 @@ class Source
 
   field :link,     type: String
   field :media_id, type: Integer
-  field :season,   type: Integer, default: 0
-  field :episode,  type: Integer, default: 0
+  field :season,   type: Integer, default: nil
+  field :episode,  type: Integer, default: nil
 
   belongs_to :media
 
-  before_update :notify_subscribers, if: :episode_released?
+  after_update :notify_subscribers, if: :released?
 
   protected
 
-  def episode_released?
+  def released?
     self.season_changed? or self.episode_changed?
   end
 
   def notify_subscribers
     self.media.subscribes.pluck(:user_id).each do |user_id|
-      SomeEmailJob.notify user_id.to_s
+      # SomeEmailJob.notify user_id.to_s
+      if premiere?
+        SomeEmailJob.notify_about_release(user_id, self.media)
+      elsif new_episode_released?
+        SomeEmailJob.notify_about_new_episode(user_id, self.media)
+      elsif season_released?
+        SomeEmailJob.notify_about_new_season(user_id, self.media)
+      end
     end
+  end
+
+  def premiere?
+    self.season_change.first.nil?
+  end
+
+  def season_released?
+    self.season_changed? and self.episode_changed?
+  end
+
+  def new_episode_released?
+    !self.season_changed? and self.episode_changed?
   end
 
 end
