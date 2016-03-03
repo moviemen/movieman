65959a399e804de9724aa04f3f1247b785d986c7
Bohdan Malets <bohdan.malets@gmail.com>
2016-03-03 18:55:21 +0200

    subscriptions tab

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -3,7 +3,7 @@ class SubscriptionsController < ApplicationController
 
   def index
     media_ids      = current_user.subscriptions.pluck(:media_id)
-    @subscriptions = Media.all_in( id: media_ids ).page(subscription_params)
+    @subscriptions = Media.where(:_id.in => media_ids).page(subscription_params[:page])
     data           = @subscriptions.map{ |subscription| render_to_string partial: 'media/subscription', locals: {subscription: subscription} }
 
     render json: {status: 200, data: data.join(''), total: @subscriptions.count}
