a93177bb3a85d6073d82558d5f503013e5f3b344
bohdankutkovy <kutkovybohdan@gmail.com>
2016-03-03 18:38:04 +0200

    finding for user subs

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -2,7 +2,8 @@ class SubscriptionsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @subscriptions = Media.movies.page(subscription_params[:page])
+    media_ids      = current_user.subscriptions.pluck(:media_id)
+    @subscriptions = Media.all_in( id: media_ids ).page(subscription_params)
     data           = @subscriptions.map{ |subscription| render_to_string partial: 'media/subscription', locals: {subscription: subscription} }
 
     render json: {status: 200, data: data.join(''), total: @subscriptions.count}
