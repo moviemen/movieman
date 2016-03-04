6a4179d31f4dfe27bd29d645b68c5523b534b1ff
bohdankutkovy <kutkovybohdan@gmail.com>
2016-03-04 18:14:35 +0200

    switcher is here

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,9 @@ class User
   field :current_sign_in_ip, type: String
   field :last_sign_in_ip,    type: String
 
+  #Notify
+  field :email_notifiable,   type: Boolean, default: false
+
   ## Confirmable
   # field :confirmation_token,   type: String
   # field :confirmed_at,         type: Time
